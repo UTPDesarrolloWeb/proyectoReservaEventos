25965e203b61e5ad358265bf731e0bac3a503532
Cristhian <CristhianPalominoU29>
2026-07-05 02:01:29 -0500

    feat: integrar modal de pago en frontend y actualizar endpoints en backend

--- microservicios/pago-service/src/main/java/com/evently/pago/client/ReservaClient.java
+++ microservicios/pago-service/src/main/java/com/evently/pago/client/ReservaClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@FeignClient(name = "reserva-service", url = "http://localhost:8083")
+@FeignClient(name = "reserva-service", url = "http://reserva-service:8083")
 public interface ReservaClient {
     @GetMapping("/api/reservas/{id}")
     ReservaDTO obtenerPorId(@PathVariable("id") Long id);
