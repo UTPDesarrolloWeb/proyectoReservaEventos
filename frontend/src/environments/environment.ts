d685871ce02028ae1220f74ca0b66d8021f3e338
nicollas <ng08082006@gmail.com>
2026-07-12 22:49:27 -0500

    Fix: update dev client ID and relax COOP policy

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api',
   apiReservas: 'http://localhost:8080/api/reservas',
-  googleClientId: '270457705795-dpnorhev66k37j275pvbh4std4bqct9c.apps.googleusercontent.com'
+  googleClientId: '270457705795-mo9rkol9ckn7ovjm40npoi0b2igp8ueb.apps.googleusercontent.com'
 };
