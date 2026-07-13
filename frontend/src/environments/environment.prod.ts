897040d9232cda3e583f2cd50c00f3edc249ba84
nicollas <ng08082006@gmail.com>
2026-07-12 21:15:30 -0500

    Update google client ID to restored version

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'API_GATEWAY_URL_PLACEHOLDER/api',
   apiReservas: 'API_GATEWAY_URL_PLACEHOLDER/api/reservas',
-  googleClientId: '270457705795-dpnorhev66k37j275pvbh4std4bqct9c.apps.googleusercontent.com'
+  googleClientId: '270457705795-mo9rkol9ckn7ovjm40npoi0b2igp8ueb.apps.googleusercontent.com'
 };
